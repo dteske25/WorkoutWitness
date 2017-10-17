6f00a029f177d9ff67cc9accf34408b1c09921ac
Daric Teske <tesked@outlook.com>
2017-10-17 17:50:25 -0500

    give it another shot

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+printf "\n\nRestoring\n\n"
+
 dotnet restore
 cd WorkoutWitness.Web
 mkdir dist
@@ -7,8 +9,12 @@ mkdir dist
 sudo npm install
 sudo npm rebuild node-sass
 
+printf "\n\nPublishing\n\n"
 sudo dotnet publish -c Release -o ./dist
-zip -r latest ./dist
-cd ..
+
+printf "\n\nZipping\n\n"
+cd dist 
+zip -r latest *
+cd ../..
 mkdir -p release
-mv WorkoutWitness.Web/latest.zip release/latest.zip
+mv WorkoutWitness.Web/dist/latest.zip release/latest.zip
