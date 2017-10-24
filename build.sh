e5837c0afefd108eaa1d8a36d31710229cd8a384
Daric Teske <tesked@outlook.com>
2017-10-24 10:15:11 -0500

    ls some stuff

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ printf "\n\n Restoring \n\n"
 dotnet restore
 cd WorkoutWitness.Web
 
+ls
+
 npm install
 npm rebuild node-sass
 
@@ -12,7 +14,12 @@ printf "\n\n Building \n\n"
 dotnet build -c Release
 
 printf "\n\n Zipping UI \n\n"
+ls
+
 cd wwwroot/dist
+
+ls
+
 zip -r latest .
 cd ../../..
 mkdir -p release
