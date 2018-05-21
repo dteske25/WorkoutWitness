2051edf18ea97d35ab8812166cef337093ef0c77
Daric Teske <dteske25@users.noreply.github.com>
2018-05-21 11:21:27 -0500

    Fix Travis-CI

--- build.sh
+++ build.sh
@@ -5,8 +5,10 @@ printf "\n\n Restoring \n\n"
 dotnet restore
 cd WorkoutWitness.Web
 
+cd ClientApp
 npm install
 npm rebuild node-sass
+cd ..
 
 printf "\n\n Building \n\n"
 dotnet build -c Release
@@ -14,4 +16,4 @@ dotnet build -c Release
 
 printf "\n\n Running Webpack \n\n"
 cd ClientApp
-npm build
+npm run-script build
